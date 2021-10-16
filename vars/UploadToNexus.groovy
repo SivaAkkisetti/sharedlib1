d857bb4a98f43a54530663b2ddbd03f0bf49a07f
SivaAkkisetti <85981615+SivaAkkisetti@users.noreply.github.com>
2021-10-16 11:39:35 +0530

    Create UploadToNexus.groovy

---
+++ vars/UploadToNexus.groovy
@@ -0,0 +1,14 @@
+def call(nexusRepo){
+                def pom = readMavenPom file: 'pom.xml'
+                def repo= nexusRepo
+                def groupId=pom.groupId
+                def artifactId=pom.artifactId
+                nexusArtifactUploader artifacts: [[artifactId:artifactId, file: "target/${articatId}-${pom.version}.war", type: 'war']],
+                credentialsId: 'admin', 
+                groupId: groupId, 
+                nexusUrl: '172.31.15.134:8081', 
+                nexusVersion: 'nexus3', 
+                protocol: 'http', 
+                repository: repo, 
+                version: pom.version
+}
