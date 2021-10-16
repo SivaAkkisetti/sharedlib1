9e26a68eccccf6cc5b82435f512c1326db8cb865
SivaAkkisetti <85981615+SivaAkkisetti@users.noreply.github.com>
2021-10-16 12:20:01 +0530

    Update UploadToNexus.groovy

--- vars/UploadToNexus.groovy
+++ vars/UploadToNexus.groovy
@@ -3,7 +3,7 @@ def call(nexusRepo){
                 def repo= nexusRepo
                 def groupId=pom.groupId
                 def artifactId=pom.artifactId
-                nexusArtifactUploader artifacts: [[artifactId:artifactId, file: "target/${articatId}-${pom.version}.war", type: 'war']],
+                nexusArtifactUploader artifacts: [[artifactId:artifactId, file: "target/${artifactId}-${pom.version}.war", type: 'war']],
                 credentialsId: 'admin', 
                 groupId: groupId, 
                 nexusUrl: '172.31.15.134:8081', 
